a4a5c767bf559de1189ec1befa925d0f0df93c4d
CristopherVA <ing.valentin99@hotmail.com>
2021-09-10 00:54:19 -0300

    sidebar complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Pages
+import { Home } from './pages/Home';
+import { Reports } from './pages/Reports';
+import { Products } from './pages/Products';
 
-export default App;
+export const App = () => {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/reports' component={Reports} />
+          <Route exact path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
+  )
+}
